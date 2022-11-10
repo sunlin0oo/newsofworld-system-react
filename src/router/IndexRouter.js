086e2e579eb3cf6a04dc4781c5652a864583ccb9
sunlinMain <sunzifeng@simverse.cn>
2022-11-10 11:13:49 +0800

    动态创建路由

--- src/router/IndexRouter.js
+++ src/router/IndexRouter.js
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import { Route, Routes } from 'react-router-dom'
 import Login from '../view/login/Login'
 import NewsSandBox from '../view/newSandBox/NewsSandBox'
@@ -8,17 +8,62 @@ import UserList from '../view/newSandBox/user-manage/UserList'
 import RoleList from '../view/newSandBox/right-manage/RoleList'
 import RightList from '../view/newSandBox/right-manage/RightList'
 import NotFound from '../view/nopermission/NotFound'
+import NewsAdd from '../view/newSandBox/news-manage/NewsAdd'
+import NewsDraft from '../view/newSandBox/news-manage/NewsDraft'
+import NewsCategory from '../view/newSandBox/news-manage/NewsCategory'
+import Audit from '../view/newSandBox/audit-manage/Audit'
+import AuditList from '../view/newSandBox/audit-manage/AuditList'
+import Unpublished from '../view/newSandBox/publish-manage/Unpublished'
+import Published from '../view/newSandBox/publish-manage/Published'
+import Sunset from '../view/newSandBox/publish-manage/Sunset'
+import axios from 'axios';
+
+// 路由哈希表
+const LocalRouterMap = {
+    "/user-manage/list": <UserList />,
+    "/right-manage/role/list": <RoleList />,
+    "/home": <Home />,
+    "/right-manage/right/list": <RightList />,
+    "/news-manage/add": <NewsAdd />,
+    "/news-manage/draft": <NewsDraft />,
+    "/news-manage/category": <NewsCategory />,
+    "/audit-manage/audit": <Audit />,
+    "/audit-manage/list": <AuditList />,
+    "/publish-manage/unpublished": <Unpublished />,
+    "/publish-manage/published": <Published />,
+    "/publish-manage/sunset": <Sunset />,
+}
 export default function IndexRouter() {
+    const [backrouteList, setBackRouteList] = useState([]);
+    useEffect(() => {
+        // promise.all()成功时，在then（result）中result是个数组
+        Promise.all([
+            axios.get(`http://localhost:8000/rights`),
+            axios.get(`http://localhost:8000/children`),
+        ]).then(res => {
+            console.log(res);
+            // 将所有的权限放在一块，扁平化处理
+            setBackRouteList([...res[0].data, ...res[1].data])
+        })
+    }, [])
     return (
         <Routes>
             <Route path='/login' element={<Login />}></Route>
             <Route path='/' element={<AuthComponent>{<NewsSandBox />}</AuthComponent>}>
-                <Route path='' element={<Redirect to='/home'></Redirect>}></Route>
-                <Route path='home' element={<Home></Home>}></Route>
+                {/* 动态创建路由 */}
+                {
+                    backrouteList.map(item =>
+                        <Route path={item.key} key={item.key} element={LocalRouterMap[item.key]}></Route>)
+                }
+                {/* <Route path='home' element={<Home></Home>}></Route>
                 <Route path='user-manage/list' element={<UserList></UserList>}></Route>
                 <Route path='right-manage/role/list' element={<RoleList></RoleList>}></Route>
-                <Route path='right-manage/right/list' element={<RightList></RightList>}></Route>
-                <Route path='*' element={<NotFound></NotFound>}></Route>
+                <Route path='right-manage/right/list' element={<RightList></RightList>}></Route> */}
+                <Route path='' element={<Redirect to='/home'></Redirect>}></Route>
+                {
+                    // 解决数据没有传输过来导致路由无法渲染的问题(会短暂的存在Notfound的图标)
+                    backrouteList.length>0 && <Route path='*' element={<NotFound></NotFound>}></Route>
+                }
             </Route>
         </Routes>
     )
