5b7265f0b69bd1c0483f2cd8326c3422c9acb03c
sunlinMain <sunzifeng@simverse.cn>
2022-11-10 23:56:11 +0800

    添加进度条Nprogress的使用

--- src/view/newSandBox/NewsSandBox.js
+++ src/view/newSandBox/NewsSandBox.js
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { useEffect } from 'react'
 import { Outlet } from 'react-router-dom'
 //components
 import SideMenu from '../../components/sanbox/SideMenu'
@@ -7,8 +7,15 @@ import TopHeader from '../../components/sanbox/TopHeader'
 import { Layout } from 'antd';
 // css
 import './newSandBox.css'
+// 进度条
+import NProgress from 'nprogress';
+import  'nprogress/nprogress.css';
 const { Content } = Layout;
 export default function NewsSandBox() {
+  NProgress.start();
+  useEffect(()=>{
+    NProgress.done();
+  })
   return (
     <Layout>
       <SideMenu></SideMenu>
