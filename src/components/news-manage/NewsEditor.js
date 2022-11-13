5cd2df80ac3bd5d7285250fc923366d46550c0a6
sunlinMain <sunzifeng@simverse.cn>
2022-11-13 20:36:21 +0800

    富文本组件解决Bug

--- src/components/news-manage/NewsEditor.js
+++ src/components/news-manage/NewsEditor.js
@@ -1,26 +1,39 @@
 import React, { useState } from 'react'
 import { Editor } from "react-draft-wysiwyg";
-import { convertToRaw } from 'draft-js';
+import { EditorState, convertToRaw, ContentState } from 'draft-js';
 import "react-draft-wysiwyg/dist/react-draft-wysiwyg.css";
 import draftToHtml from 'draftjs-to-html';
+import htmlToDraft from 'html-to-draftjs'
+import { useEffect } from 'react';
 
 export default function NewsEditor(props) {
-    const [editorState, setEditorState] = useState('')
+    const [editorState, setEditorState] = useState('');
+    useEffect(() => {
+        const html = props.content
+        // 解决小Bug
+        if(html === undefined) return;
+        const contentBlock = htmlToDraft(html);
+        if (contentBlock) {
+            const contentState = ContentState.createFromBlockArray(contentBlock.contentBlocks);
+            setEditorState(EditorState.createWithContent(contentState))
+        }
+    }, [props.content])
+
     return (
         <div>
             <Editor
-            // 编辑的存储值 与 下面的Change函数进行绑定
+                // 编辑的存储值 与 下面的Change函数进行绑定
                 editorState={editorState}
                 // 对于编辑器每一块Class的引用
                 toolbarClassName="toolbarClassName"
                 wrapperClassName="wrapperClassName"
                 editorClassName="editorClassName"
                 // Editor是受控组件，需要通过Change函数进行实时更新
-                onEditorStateChange={(editorState)=>{
+                onEditorStateChange={(editorState) => {
                     setEditorState(editorState)
                 }}
                 // 失去焦点,会触发change函数
-                onBlur={()=>{
+                onBlur={() => {
                     // console.log(draftToHtml(convertToRaw(editorState.getCurrentContent())))
                     props.getContent(draftToHtml(convertToRaw(editorState.getCurrentContent())));
                 }}
