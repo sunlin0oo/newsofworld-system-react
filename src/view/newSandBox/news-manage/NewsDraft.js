7a30571419adb6b41cbe721a2482a2d4bd4e408a
sunlinMain <sunzifeng@simverse.cn>
2022-11-13 19:46:37 +0800

    草稿箱功能

--- src/view/newSandBox/news-manage/NewsDraft.js
+++ src/view/newSandBox/news-manage/NewsDraft.js
@@ -1,7 +1,92 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
+import { Table, Tag, Button, Modal } from 'antd'
+import { DeleteOutlined, EditOutlined, ExclamationCircleOutlined, VerticalAlignTopOutlined } from '@ant-design/icons';
+import WithRouter from '../../../components/WithRouter';
+import axios from 'axios';
+const { confirm } = Modal;
+const { username } = JSON.parse(localStorage.getItem('token'));
+function NewsDraft(props) {
+  const [dataSource, setDataSource] = useState([]);
 
-export default function NewsDraft() {
+  useEffect(() => {
+    axios.get(`/news?author=${username}&auditState=0&_expand=category`).then(res => {
+      console.log(res.data);
+      const list = res.data;
+      setDataSource(list);
+    })
+  }, [])
+
+  const columns = [
+    {
+      title: 'ID',
+      dataIndex: 'id',
+      key: 'id',
+    },
+    {
+      title: '新闻标题',
+      dataIndex: 'title',
+      key: 'title',
+      render:(title,item)=>{
+        // 跳转到新闻的详细界面
+        return <a href={`#/news-manage/preview/${item.id}`}>{title}</a>
+      }
+    },
+    {
+      title: '作者',
+      dataIndex: 'author',
+      key: 'author',
+      render: (key) => {
+        return <Tag color="blue" key={key}>{key}</Tag>
+      }
+    },
+    {
+      title: '新闻分类',
+      dataIndex: 'category',
+      key: 'category',
+      render: (category) => {
+        return <Tag color="blue" key={category}>{category.title}</Tag>
+      }
+    },
+    {
+      title: '操作',
+      // 什么都不写的话会直接获取到这一项
+      render: (item) => {
+        return <div>
+          <Button type='primary' shape="circle" icon={<EditOutlined />} onClick={()=>props.history.push(`/news-manage/update/${item.id}`)}/>
+          <Button danger shape="circle" size={'large'} icon={<DeleteOutlined />} onClick={() => delConfirm(item)} />
+          <Button type='primary' shape="circle" size={'large'} icon={<VerticalAlignTopOutlined />} onClick={() => delConfirm(item)} />
+        </div>
+      }
+    },
+  ];
+  // 确认删除框==>对话框
+  const delConfirm = (item) => {
+    confirm({
+      title: 'Do you Want to delete these items?',
+      icon: <ExclamationCircleOutlined />,
+      content: 'Some descriptions',
+      onOk() {
+        deleteMethod(item)
+      },
+      onCancel() {
+        console.log('Cancel');
+      },
+    });
+  }
+
+  const deleteMethod = (item) => {
+    // 前后端同步
+    setDataSource(dataSource.filter(data => data.id !== item.id));// 过滤出与删除的id不相同的数据
+    axios.delete(`/news/${item.id}`);
+  }
   return (
-    <div>NewsDraft</div>
+    <div>
+      <Table dataSource={dataSource} columns={columns}
+        pagination={{
+          pageSize: 4
+        }} 
+        rowKey={item=>item.id}/>
+    </div>
   )
 }
+export default WithRouter(NewsDraft)
