8f32b83cdfdd5e8e9f6f2d10e8dd62837f688924
sunlinMain <sunzifeng@simverse.cn>
2022-11-15 00:45:08 +0800

    已发布功能

--- src/view/newSandBox/publish-manage/Published.js
+++ src/view/newSandBox/publish-manage/Published.js
@@ -1,7 +1,15 @@
 import React from 'react'
+import NewPublish from '../../../components/publish-manage/NewPublish'
+import usePublish from '../../../components/publish-manage/UsePublish'
+import { Button} from 'antd'
 
-export default function Published() {
+export default function Unpublished() {
+  const {dataSource, handleSunset} = usePublish(2)
+
   return (
-    <div>Published</div>
+    <div>
+      <NewPublish dataSource={dataSource} button={(id)=><Button danger
+      onClick={()=>handleSunset(id)}>下线</Button>}></NewPublish>
+    </div>
   )
 }
