6acb64792474a55f24ad33edd2e143ed72c08766
sunlinMain <sunzifeng@simverse.cn>
2022-11-15 00:45:02 +0800

    待发布功能

--- src/view/newSandBox/publish-manage/Unpublished.js
+++ src/view/newSandBox/publish-manage/Unpublished.js
@@ -1,20 +1,21 @@
-import axios from 'axios'
 import React from 'react'
-import { useState } from 'react'
-import { useEffect } from 'react'
 import NewPublish from '../../../components/publish-manage/NewPublish'
+import usePublish from '../../../components/publish-manage/UsePublish'
+import { Button} from 'antd'
 
 export default function Unpublished() {
-  const [dataSource, setDataSource] = useState([])
-  const {username} = JSON.parse(localStorage.getItem('token'))
-  useEffect(()=>{
-    axios.get(`/news?author=${username}&publishState=1&_expand=category`).then(res=>{
-      setDataSource(res.data)
-    })
-  },[username])
+  // const [dataSource, setDataSource] = useState([])
+  // const {username} = JSON.parse(localStorage.getItem('token'))
+  // useEffect(()=>{
+  //   axios.get(`/news?author=${username}&publishState=1&_expand=category`).then(res=>{
+  //     setDataSource(res.data)
+  //   })
+  // },[username])
+  const {dataSource, handlePublish} = usePublish(1)
   return (
     <div>
-      <NewPublish dataSource={dataSource}></NewPublish>
+      <NewPublish dataSource={dataSource} button={(id)=><Button type='primary'
+      onClick={()=>handlePublish(id)}>发布</Button>}></NewPublish>
     </div>
   )
 }
