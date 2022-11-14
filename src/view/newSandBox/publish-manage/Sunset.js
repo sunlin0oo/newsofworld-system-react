f7dccb073dc64d26067a78f64b40c221a7142a0b
sunlinMain <sunzifeng@simverse.cn>
2022-11-15 00:45:39 +0800

    已下线组件

--- src/view/newSandBox/publish-manage/Sunset.js
+++ src/view/newSandBox/publish-manage/Sunset.js
@@ -1,7 +1,14 @@
 import React from 'react'
+import NewPublish from '../../../components/publish-manage/NewPublish'
+import usePublish from '../../../components/publish-manage/UsePublish'
+import { Button} from 'antd'
 
 export default function Sunset() {
+  const {dataSource, handleDelete} = usePublish(3)
   return (
-    <div>Sunset</div>
+    <div>
+      <NewPublish dataSource={dataSource} button={(id)=><Button danger
+      onClick={()=>handleDelete(id)}>删除</Button>}></NewPublish>
+    </div>
   )
 }
