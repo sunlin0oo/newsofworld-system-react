cfd56432fb2d0567adced0fc6a61db7253031c65
sunlinMain <sunzifeng@simverse.cn>
2022-11-15 00:45:32 +0800

    公用Hooks

--- src/components/publish-manage/UsePublish.js
+++ src/components/publish-manage/UsePublish.js
@@ -1,22 +1,37 @@
 import axios from 'axios'
-import { useState } from 'react'
-import { useEffect } from 'react'
+import { useState, useEffect } from 'react'
 function usePublish(type){
     const [dataSource, setDataSource] = useState([])
     const {username} = JSON.parse(localStorage.getItem('token'))
     useEffect(()=>{
       axios.get(`/news?author=${username}&publishState=${type}&_expand=category`).then(res=>{
-        console.log('res.data', res.data)
+        // console.log('res.data', res.data)
         setDataSource(res.data)
       })
     },[username, type])
-    return (
-        dataSource
-    )
-    // 这样返回一个dataSource属性
-    // return {
+    const handlePublish = (id) =>{
+        console.log('handlePublish', id)
+    }
+    const handleSunset = (id) =>{
+        console.log('handleSunset', id)
+    }
+    const handleDelete = (id) =>{
+        console.log('handleDelete', id)
+    }
+    // 这样直接导出数值
+    // return (
+    //     handleDelete,
+    //     handleSunset,
+    //     handlePublish,
     //     dataSource
-    // }
+    // )
+    // 这样返回一个dataSource属性
+    return {
+        dataSource,
+        handleDelete,
+        handleSunset,
+        handlePublish
+    }
 }
 
 export default usePublish
