f5d11d45e4ef3c32e876b2a76364f8713c4fe1d2
sunlinMain <sunzifeng@simverse.cn>
2022-11-15 00:16:00 +0800

    待发布功能

--- src/view/newSandBox/publish-manage/Unpublished.js
+++ src/view/newSandBox/publish-manage/Unpublished.js
@@ -1,7 +1,20 @@
+import axios from 'axios'
 import React from 'react'
+import { useState } from 'react'
+import { useEffect } from 'react'
+import NewPublish from '../../../components/publish-manage/NewPublish'
 
 export default function Unpublished() {
+  const [dataSource, setDataSource] = useState([])
+  const {username} = JSON.parse(localStorage.getItem('token'))
+  useEffect(()=>{
+    axios.get(`/news?author=${username}&publishState=1&_expand=category`).then(res=>{
+      setDataSource(res.data)
+    })
+  },[username])
   return (
-    <div>Unpublished</div>
+    <div>
+      <NewPublish dataSource={dataSource}></NewPublish>
+    </div>
   )
 }
