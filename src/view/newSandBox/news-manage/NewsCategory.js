e034ced34b98949f4709c6e5300dca70ee1acd2c
sunlinMain <sunzifeng@simverse.cn>
2022-11-14 23:56:15 +0800

    新闻分类的功能完成

--- src/view/newSandBox/news-manage/NewsCategory.js
+++ src/view/newSandBox/news-manage/NewsCategory.js
@@ -1,11 +1,11 @@
-import React, { useEffect, useState } from 'react'
-import { Table, Tag, Button, Modal, Popover, Switch } from 'antd'
-import { DeleteOutlined, EditOutlined, ExclamationCircleOutlined } from '@ant-design/icons';
+import React, { useEffect, useState, useContext, useRef } from 'react'
+import { Table, Button, Modal,Form, Input,} from 'antd'
+import { DeleteOutlined, ExclamationCircleOutlined } from '@ant-design/icons';
 import axios from 'axios';
 const { confirm } = Modal;
 export default function NewsCategory() {
   const [dataSource, setDataSource] = useState([]);
-
+  const EditableContext = React.createContext(null)
   useEffect(() => {
     axios.get('/categories').then(res => {
       console.log('NewsCategory-res.data', res.data);
@@ -14,6 +14,25 @@ export default function NewsCategory() {
     })
   }, [])
 
+  const handleSave = (record) =>{
+    console.log(record);
+    setDataSource(dataSource.map(item=>{
+      // 如果更新的ID 与 遍历的ID一致，则返回新设置的,否则是原先的
+      if(item.id === record.id){
+        return {
+          id:item.id,
+          title:record.title,
+          value:record.title
+        }
+      }
+      return item
+    }))
+    axios.patch(`/categories/${record.id}`,{
+      title:record.title,
+      value:record.title
+    })
+  }
+
   const columns = [
     {
       title: 'ID',
@@ -21,57 +40,28 @@ export default function NewsCategory() {
       key: 'id',
     },
     {
-      title: '权限名称',
+      title: '栏目名称',
       dataIndex: 'title',
       key: 'title',
-
-    },
-    {
-      title: '权限路径',
-      dataIndex: 'key',
-      key: 'key',
-      render: (key) => {
-        return <Tag color="blue" key={key}>{key}</Tag>
-      }
+      onCell: (record) => ({
+        record,
+        editable: true,
+        dataIndex: 'title',
+        title: '栏目名称',
+        handleSave,
+      }),
     },
     {
       title: '操作',
       // 什么都不写的话会直接获取到这一项
       render: (item) => {
         return <div>
-          <Popover
-            content={<div style={{ textAlign: 'center' }}>
-              {/* 根据后端控制权限 */}
-              <Switch checked={item.pagepermisson} onChange={() => switchMethod(item)}></Switch>
-            </div>}
-            title="页面配置项"
-            trigger={item.pagepermisson === undefined ? "null" : "click"}
-          // onOpenChange={handleOpenChange}
-          >
-            {/*  */}
-            <Button type='primary' shape="circle" icon={<EditOutlined />} disabled={item.pagepermisson === undefined} />
-          </Popover>
           <Button danger shape="circle" size={'large'} icon={<DeleteOutlined />} onClick={() => delConfirm(item)} />
         </div>
       }
     },
   ];
-  const switchMethod = (item) => {
-    // 存在引用关系，则会一直受影响
-    item.pagepermisson = item.pagepermisson === 1 ? 0 : 1;
-    // console.log(ite)
-    // 强制更新
-    setDataSource([...dataSource]);
-    if ((item.grade === 1)) {
-      axios.patch(`/rights/${item.id}`, {
-        pagepermisson: item.pagepermisson,
-      })
-    } else {
-      axios.patch(`/children/${item.id}`, {
-        pagepermisson: item.pagepermisson,
-      })
-    }
-  }
+
   // 确认删除框==>对话框
   const delConfirm = (item) => {
     confirm({
@@ -88,32 +78,101 @@ export default function NewsCategory() {
   }
 
   const deleteMethod = (item) => {
-    console.log(item);
-    // 当前页面同步状态 + 后端同步
-    // 根据grade判断层级字段
-    if (item.grade === 1) {
-      // 这是删除一级菜单的情况
-      // 首先处理页面状态同步
       setDataSource(dataSource.filter(data => data.id !== item.id));// 过滤出与删除的id不相同的数据
       // 在处理后端
-      axios.delete(`/rights/${item.id}`)
-    } else {
-      // 思考新的实现方法！！！
-      let list = dataSource.filter(data => data.id === item.rightId)// 去上级找到要删除的项,拿到的是父级的字段(一级菜单信息)
-      console.log(list);
-      list[0].children = list[0].children.filter(data => data.id !== item.id)// 去替换二级菜单，过滤掉不要的，过滤出要的
-      // 这样的操作也会影响到dataSource，因为不是深复制，会导致改变
-      setDataSource([...dataSource]);
-      axios.delete(`/children/${item.id}`)
-    }
-
+      axios.delete(`/categories/${item.id}`)
   }
+
+  const EditableRow = ({ index, ...props }) => {
+    const [form] = Form.useForm();
+    return (
+      <Form form={form} component={false}>
+        <EditableContext.Provider value={form}>
+          <tr {...props} />
+        </EditableContext.Provider>
+      </Form>
+    );
+  };
+  const EditableCell = ({
+    title,
+    editable,
+    children,
+    dataIndex,
+    record,
+    handleSave,
+    ...restProps
+  }) => {
+    const [editing, setEditing] = useState(false);
+    const inputRef = useRef(null);
+    const form = useContext(EditableContext);
+    useEffect(() => {
+      if (editing) {
+        inputRef.current.focus();
+      }
+    }, [editing]);
+    const toggleEdit = () => {
+      setEditing(!editing);
+      form.setFieldsValue({
+        [dataIndex]: record[dataIndex],
+      });
+    };
+    const save = async () => {
+      try {
+        const values = await form.validateFields();
+        toggleEdit();
+        handleSave({
+          ...record,
+          ...values,
+        });
+      } catch (errInfo) {
+        console.log('Save failed:', errInfo);
+      }
+    };
+    let childNode = children;
+    if (editable) {
+      childNode = editing ? (
+        <Form.Item
+          style={{
+            margin: 0,
+          }}
+          name={dataIndex}
+          rules={[
+            {
+              required: true,
+              message: `${title} is required.`,
+            },
+          ]}
+        >
+          <Input ref={inputRef} onPressEnter={save} onBlur={save} />
+        </Form.Item>
+      ) : (
+        <div
+          className="editable-cell-value-wrap"
+          style={{
+            paddingRight: 24,
+          }}
+          onClick={toggleEdit}
+        >
+          {children}
+        </div>
+      );
+    }
+    return <td {...restProps}>{childNode}</td>;
+  };
+
   return (
     <div>
       {/* 为什么这里不用加rowkey是因为，后端传来的字段，存在key字段，故自动填充上 */}
       <Table dataSource={dataSource} columns={columns}
         pagination={{
           pageSize: 4
+        }}
+        rowKey={item => item.id}
+        components={{
+          body: {
+            row: EditableRow,
+            cell: EditableCell,
+          },
         }} />
     </div>
   )
