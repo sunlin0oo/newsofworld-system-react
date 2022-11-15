b5fc9623fc9b74ff5105ea2042445ea85aa8d77b
sunlinMain <sunzifeng@simverse.cn>
2022-11-16 00:22:57 +0800

    axios状态拦截

--- src/util/http.js
+++ src/util/http.js
@@ -1,5 +1,6 @@
 // 对于路径请求的封装==>以后改装成redux后的操作空间==>需要对于redux的操作进行使用
 import axios from "axios";
+import store from '../redux/store'
 // 省略路径
 axios.defaults.baseURL = 'http://localhost:8000';
 
@@ -7,3 +8,34 @@ axios.defaults.baseURL = 'http://localhost:8000';
 
 // axios.interceptors.request.use
 // axios.interceptors.response.use
+
+// Add a request interceptor
+axios.interceptors.request.use(function (config) {
+    // Do something before request is sent
+    // 显示loding==>请求中
+    store.dispatch({
+      type:'change_loading',
+      payload:true
+    })
+    return config;
+  }, function (error) {
+    // Do something with request error
+    return Promise.reject(error);
+  });
+
+// Add a response interceptor
+axios.interceptors.response.use(function (response) {
+    // Any status code that lie within the range of 2xx cause this function to trigger
+    // Do something with response data
+    // 请求成功==>隐藏loading
+    store.dispatch({
+      type:'change_loading',
+      payload:false
+    })
+    return response;
+  }, function (error) {
+    // Any status codes that falls outside the range of 2xx cause this function to trigger
+    // Do something with response error
+    // 请求失败==>隐藏loading
+    return Promise.reject(error);
+  });
