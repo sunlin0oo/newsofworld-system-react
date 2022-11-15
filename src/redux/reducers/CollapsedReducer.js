bfd1ea007be777a089c7201d2ea1731a8ba8a84b
sunlinMain <sunzifeng@simverse.cn>
2022-11-15 23:53:02 +0800

    侧边栏reducer

---
+++ src/redux/reducers/CollapsedReducer.js
@@ -0,0 +1,17 @@
+// 管理侧边栏伸缩的状态
+// 需要定义初始状态
+export const CollapsedReducer = (prevState = {
+    isCollapsed: true
+}, action) => {
+    let { type } = action;
+
+    switch (type) {
+        case 'change_collapsed':
+            let newState = { ...prevState }
+            newState.isCollapsed = !newState.isCollapsed
+            return newState
+        default:
+            return prevState
+
+    }
+}
