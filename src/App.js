bd1eec38f159154e5dd30a0541109a3ba66bc9d3
sunlinMain <sunzifeng@simverse.cn>
2022-11-16 00:36:11 +0800

    持久化状态修改

--- src/App.js
+++ src/App.js
@@ -3,14 +3,18 @@ import { HashRouter, } from 'react-router-dom'
 import './App.css'
 import IndexRouter from './router/IndexRouter';
 import { Provider } from 'react-redux';
-import store from './redux/store';
+import {store, persistor} from './redux/store';
+import { PersistGate } from 'redux-persist/integration/react'
+
 export default function App() {
   // 通过Provider对store进行传递==>所有Connect包装的组件都可以轻松的获取到store
   return (
     <Provider store={store}>
+      <PersistGate loading={null} persistor={persistor}>
       <HashRouter>
         <IndexRouter></IndexRouter>
       </HashRouter>
+      </PersistGate>
     </Provider>
 
   )
