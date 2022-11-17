d6e1c8ad105c6f5b3be8665ff4c62ed4d3762d9c
sunlinMain <sunzifeng@simverse.cn>
2022-11-17 23:29:43 +0800

    柱状图优化及添加饼状图

--- src/view/newSandBox/home/Home.js
+++ src/view/newSandBox/home/Home.js
@@ -1,13 +1,19 @@
 import React, { useEffect, useRef, useState } from 'react'
-import { Card, Col, List, Row, Avatar } from 'antd';
+import { Card, Col, List, Row, Avatar, Drawer } from 'antd';
 import { EditOutlined, EllipsisOutlined, SettingOutlined } from '@ant-design/icons';
 import axios from 'axios'
-import  * as Echarts from 'echarts'
+import * as Echarts from 'echarts'
 import _ from 'lodash'
 const { Meta } = Card;
 export default function Home() {
   const [dataViewSource, setDataViewSource] = useState([]);
   const [dataStarSource, setDataStarSource] = useState([]);
+  const [allDataSource, setAllDataSource] = useState([]);
+  const [visible, setVisible] = useState(false);
+  const [pieChar, setPieChar] = useState(null);
+  const barRef = useRef();
+  const pieRef = useRef();
+
   // 解决了跨域问题==>基于node进行处理
   // const ajax=()=>{
   //   // 取数据
@@ -54,27 +60,34 @@ export default function Home() {
   //   })
   // }
   // 获取到降序的访问数
-  useEffect(()=>{
-    axios.get('/news?publishState=2&_expand=category&_sort=view&_order=desc&_limit=6').then(res=>{
+  useEffect(() => {
+    axios.get('/news?publishState=2&_expand=category&_sort=view&_order=desc&_limit=6').then(res => {
       setDataViewSource(res.data)
     })
-  },[]);
-  // 获取到降序的点赞数
-  useEffect(()=>{
-      axios.get('/news?publishState=2&_expand=category&_sort=star&_order=desc&_limit=6').then(res=>{
-        console.log(res.data)
-        setDataStarSource(res.data)
-      })
-    },[]);
+  }, []);
 
-  const barRef = useRef()
-    useEffect(()=>{
-      // 所有发布的新闻
-      axios.get('/news?publishState=2&_expand=category').then(res=>{
-        // console.log(_.groupBy((res.data),item=>item.category.title)) // 以category.title进行分组
-        renderBarView(_.groupBy((res.data),item=>item.category.title))
-      })
-    },[])
+  // 获取到降序的点赞数
+  useEffect(() => {
+    axios.get('/news?publishState=2&_expand=category&_sort=star&_order=desc&_limit=6').then(res => {
+      console.log(res.data)
+      setDataStarSource(res.data)
+    })
+  }, []);
+
+  // 柱状图
+  useEffect(() => {
+    // 所有发布的新闻
+    axios.get('/news?publishState=2&_expand=category').then(res => {
+      // console.log(_.groupBy((res.data),item=>item.category.title)) // 以category.title进行分组
+      renderBarView(_.groupBy((res.data), item => item.category.title));
+      setAllDataSource(res.data);
+    })
+
+    return () => {
+      window.onresize = null; // 清楚resize事件，防止频繁调用
+    }
+  }, []);
+
   const renderBarView = (obj) => {
     // 基于准备好的dom，初始化echarts实例
     var myChart = Echarts.init(barRef.current);
@@ -89,23 +102,94 @@ export default function Home() {
         data: ['数量']
       },
       xAxis: {
-        data: Object.keys(obj)
+        data: Object.keys(obj),
+        axisLabel: {
+          rotate: '60',
+          // interval:0
+        }
       },
-      yAxis: {},
+      yAxis: {
+        minInterval: 1
+      },
       series: [
         {
           name: '数量',
           type: 'bar',
-          data: Object.values(obj).map(item=>item.length)
+          data: Object.values(obj).map(item => item.length)
         }
       ]
     };
 
     // 使用刚指定的配置项和数据显示图表。
     myChart.setOption(option);
+    window.onresize = () => {
+      myChart.resize();
+    }
   }
 
-  const {username,region , role:{roleName}} = JSON.parse(localStorage.getItem('token'))
+  const renderPieView = (obj) => {
+    // 数据处理
+    let currentList = allDataSource.filter(item=>item.author === username);
+    // console.log(currentList);
+    let groupObj = _.groupBy(currentList,item=>item.category.title);
+
+    const list = [];
+    for(let name in groupObj){
+      list.push({
+        name,
+        value:groupObj[name].length
+      })
+    }
+    // 基于准备好的dom，初始化echarts实例
+    let myChart;
+    if(!pieChar){
+      myChart = Echarts.init(pieRef.current);
+      setPieChar(myChart);
+    }else{
+      myChart = pieChar;
+    }
+
+    const option = {
+      title: {
+        text: '当前用户新闻分布图示',
+        subtext: 'Fake Data',
+        left: 'center'
+      },
+      tooltip: {
+        trigger: 'item'
+      },
+      legend: {
+        orient: 'vertical',
+        left: 'left'
+      },
+      series: [
+        {
+          name: '发布数量',
+          type: 'pie',
+          radius: '50%',
+          data: list,
+          emphasis: {
+            itemStyle: {
+              shadowBlur: 10,
+              shadowOffsetX: 0,
+              shadowColor: 'rgba(0, 0, 0, 0.5)'
+            }
+          }
+        }
+      ]
+    };
+
+    option && myChart.setOption(option);
+    window.onresize = () => {
+      myChart.resize();
+    }
+  }
+
+  const onClose = () => {
+    setVisible(false);
+  };
+
+  const { username, region, role: { roleName } } = JSON.parse(localStorage.getItem('token'))
   return (
     <div>
       <Row gutter={16}>
@@ -139,7 +223,13 @@ export default function Home() {
               />
             }
             actions={[
-              <SettingOutlined key="setting" />,
+              <SettingOutlined key="setting" onClick={() => {
+                // DOM节点是异步创建，所以需要用定时器暂缓异步进行
+                setVisible(true);
+                setTimeout(() => {
+                  renderPieView();
+                }, 0)
+              }} />,
               <EditOutlined key="edit" />,
               <EllipsisOutlined key="ellipsis" />,
             ]}
@@ -150,16 +240,20 @@ export default function Home() {
               title={username}
               description={
                 <div>
-                  <b>{region?region:'全球'}</b>
-                  <span style={{padding:'30px'}}>{roleName}</span>
+                  <b>{region ? region : '全球'}</b>
+                  <span style={{ padding: '30px' }}>{roleName}</span>
                 </div>
               }
             />
           </Card>
         </Col>
       </Row>
-      <div ref={barRef}id="main" style={{width:'90%',height:'400px', marginTop:'30px'}}></div>
+      <Drawer width={'500px'} title="个人新闻分类" placement="right" closable={true} open={visible} onClose={onClose}  >
+        <div ref={pieRef} id="main" style={{ width: '90%', height: '400px', marginTop: '30px' }}></div>
+      </Drawer>
+      <div ref={barRef} id="main" style={{ width: '90%', height: '400px', marginTop: '30px' }}></div>
+
     </div>
-    
+
   )
 }
